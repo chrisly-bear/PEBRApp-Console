8cd74bd9df7898310589c3d56fd92d372624d610
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-08-03 10:58:27 +0200

    First commit (fork from google/flutter-desktop-embedding).

---
+++ macos/Flutter/flutter_export_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/chris/Dev/dev-tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/chris/Downloads/flutter-desktop-embedding-master/example"
+export "FLUTTER_TARGET=/Users/chris/Downloads/flutter-desktop-embedding-master/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Users/chris/Dev/dev-tools/flutter/bin/cache/artifacts/engine/darwin-x64"
