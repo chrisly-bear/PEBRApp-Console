03c720537af7c81ac6a81b26231fdf0aaa36511c
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-08-06 13:56:23 +0200

    Fixed widget test compile error.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,9 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pebrapp_console/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(PEBRAppConsole());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
