4922db41415bd5376dbcf8fe03c97325887ef854
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-12-13 14:17:35 +0100

    Add purple theme

--- lib/themes.dart
+++ lib/themes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color _pebraPurple = Color(0xff004768);
+const Color _pebraPurpleDark = Color(0xff002f45);
+const Color _pebraPurpleDarker = Color(0xff001c29);
 const Color _pebraRed = Color(0xfff97263);
 
 /// Get light theme.
@@ -9,7 +11,7 @@ ThemeData get lightTheme => ThemeData.light().copyWith(
       fontFamily: 'Roboto',
     ),
     primaryColor: _pebraPurple,
-    // spinner
+    // spinner & highlights
     accentColor: _pebraPurple,
     // floating action button
     floatingActionButtonTheme: ThemeData.light().floatingActionButtonTheme.copyWith(
@@ -25,6 +27,27 @@ ThemeData get lightTheme => ThemeData.light().copyWith(
     ),
 );
 
+/// Get purple theme.
+ThemeData get purpleTheme => ThemeData(
+  fontFamily: 'Roboto',
+  // header
+  primaryColor: _pebraPurpleDarker,
+  // brightness
+  brightness: Brightness.dark,
+  // background of main screen
+  scaffoldBackgroundColor: _pebraPurpleDark,
+  // dialog background
+  dialogBackgroundColor: _pebraPurple,
+  // cards
+  cardColor: _pebraPurple,
+  // spinner & highlights
+  accentColor: _pebraRed,
+  // active checkboxes
+  toggleableActiveColor: _pebraRed,
+  // button
+  buttonColor: _pebraRed,
+);
+
 /// Get dark theme.
 ThemeData get darkTheme => ThemeData.dark().copyWith(
     textTheme: ThemeData.dark().textTheme.apply(
@@ -35,6 +58,7 @@ ThemeData get darkTheme => ThemeData.dark().copyWith(
 /// Get a map of the name of each available theme along with the theme itself.
 Map<String, ThemeData> get allThemes => {
   'light': lightTheme,
+  'purple': purpleTheme,
   'dark': darkTheme,
 };
 
